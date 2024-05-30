e342ea5c8c985b17172b4821bf5e18aff0e9a513
Tomás Pérez Zorraquín <131702946+tperezz@users.noreply.github.com>
2024-05-30 16:02:30 -0300

    Funcion para manejo de errores
    Falta agregarla

--- Asistente_personal.py
+++ Asistente_personal.py
@@ -1,4 +1,4 @@
-# Importo bibliotecas necesarias
+#Importo las librerias necesarias
 import speech_recognition as sr
 import pyttsx3
 import webbrowser
@@ -9,47 +9,57 @@ import locale
 locale.setlocale(locale.LC_TIME, 'es_ES')
 
 
+def manejar_error_voz(error):
+    '''Funcion para manejar errores de reconocimiento de voz'''
+    if isinstance(error, sr.UnknownValueError):
+        return "Lo siento, no entendí eso."
+    elif isinstance(error, sr.RequestError):
+        return "Lo siento, ha ocurrido un error en la conexión."
+    else:
+        return "Lo siento, ha ocurrido un error desconocido."
+
+
 class Asistente():
-    """Clase que representa a un asistente de voz personal"""
+    '''Clase que representa a un asistente de voz personal'''
     def __init__(self):
-        """Inicia una nueva instancia del asistente"""
+        '''Inicia una nueva instancia del asistente'''
         self.engine = pyttsx3.init()
         self.running = True
         self.callado = False
         self.saludar()
         
     def speak(self, text):
-        """Funcion para que el asistente hable"""
+        '''Funcion para que el asistente hable'''
         self.engine.say(text)
         self.engine.runAndWait()
 
     def saludar(self):
-        """Funcion que saluda al iniciar el asistente"""
+        '''Funcion que saluda al iniciar el asistente'''
         self.speak('Hola Tomás, soy Luna. ¿Cómo estás? ¿Necesitas algo?')
         self.escuchar()
     
     def despedir(self):
-        """Funcion para darse cuenta cuando el asistente deja de escuchar"""
+        '''Funcion para darse cuenta cuando el asistente deja de escuchar'''
         self.speak('Adiós')
 
     def escuchar(self):
-        """Funcion que reconoce lo que el usuario dice por el microfono y lo transforma a texto en minusculas"""
+        '''Funcion que reconoce lo que el usuario dice por el microfono y lo transforma a texto en minusculas'''
         recognizer = sr.Recognizer()
         with sr.Microphone() as source:
-            print("Escuchando...")
+            print('Escuchando...')
             audio = recognizer.listen(source, timeout=30)
         try:
             texto = recognizer.recognize_google(audio, language='es-ES')
             texto = texto.lower()
-            print("Se reconoció:", texto)
+            print('Se reconoció:', texto)
             self.procesar_comando(texto)
         except sr.UnknownValueError:
-            self.speak("Lo siento, no entendí eso.")
+            self.speak('Lo siento, no entendí eso.')
         except sr.RequestError:
-            self.speak("Lo siento, ha ocurrido un error en la conexión.")
+            self.speak('Lo siento, ha ocurrido un error en la conexión.')
             
     def procesar_comando(self, comando):
-            """Funcion que contiene todos los comandos de texto que puede recibir el asistente y ejecuta sus respectivas funciones."""
+            '''Funcion que contiene todos los comandos de texto que puede recibir el asistente y ejecuta sus respectivas funciones.'''
             if 'luna' in comando:
                 self.speak('Aquí estoy')
             elif'clima' in comando:
@@ -66,12 +76,12 @@ class Asistente():
                 self.buscar_en_google()
             elif 'fecha' in comando:
                 hoy = datetime.now()
-                fecha = hoy.strftime("%d de %B") 
-                dia = hoy.strftime("%A")
+                fecha = hoy.strftime('%d de %B') 
+                dia = hoy.strftime('%A')
                 self.speak(f'Hoy es {dia} {fecha}')
             elif 'hora' in comando:
                 hoy = datetime.now()
-                hora = hoy.strftime("%H y %M")
+                hora = hoy.strftime('%H y %M')
                 self.speak(f'Son las {hora}')
             elif 'timer' in comando or 'cronómetro' in comando:
                 self.timer()
@@ -83,14 +93,14 @@ class Asistente():
                 self.speak('Perdon, no entendí el comando')
     
     def silencio(self):
-        """Funcion para que "duerma" el asistente excepto que se llame"""
+        '''Funcion para que "duerma" el asistente excepto que se llame'''
         recognizer = sr.Recognizer()
         with sr.Microphone() as source:
-            print("Modo silencio... (presiona Ctrl+C para salir)")
-            audio = recognizer.listen(source, timeout = 300)
+            print('Modo silencio... (presiona Ctrl+C para salir)')
+            audio = recognizer.listen(source, timeout=300)
         try:
             texto = recognizer.recognize_google(audio, language='es-ES')
-            print("Se reconoció:", texto)
+            print('Se reconoció:', texto)
             if 'luna' in texto or 'escuchame' in texto:
                 self.callado = False
                 self.speak('Hola de vuelta Tomás. ¿Qué necesitas?')
@@ -98,12 +108,12 @@ class Asistente():
                 self.running = False
                 return
         except sr.UnknownValueError:
-            print("Silencio mantenido...")
+            print('Silencio mantenido...')
         except sr.RequestError:
-            self.speak("Lo siento, ha ocurrido un error en la conexión.")
+            self.speak('Lo siento, ha ocurrido un error en la conexión.')
     
     def timer(self):
-        """Funcion que funciona como temporizador"""
+        '''Funcion que funciona como temporizador'''
         self.speak('Temporizador. ¿En cuánto tiempo quieres que te avise?')
         recognizer = sr.Recognizer()
         
@@ -135,45 +145,45 @@ class Asistente():
         except sr.UnknownValueError:
             self.speak('Lo siento, no entendí eso.')
         except sr.RequestError:
-            self.speak("Lo siento, ha ocurrido un error en la conexión.")
+            self.speak('Lo siento, ha ocurrido un error en la conexión.')
 
-    def transcribir(self):
-            """Funcion para transcribir lo que se le dicta"""
+        def transcribir(self):
+            '''Funcion para transcribir lo que se le dicta'''
             recognizer = sr.Recognizer()
             with sr.Microphone() as source:
-                print("Escuchando...")
+                print('Escuchando...')
                 audio = recognizer.listen(source, timeout=30)
             try:
                 texto = recognizer.recognize_google(audio, language='es-ES')
-                print("Audio transcripto: ", texto)
+                print('Audio transcripto: ', texto)
             except sr.UnknownValueError:
-                self.speak("Lo siento, no entendí eso.")
+                self.speak('Lo siento, no entendí eso.')
                 self.transcribir()
             except sr.RequestError:
-                self.speak("Lo siento, ha ocurrido un error en la conexión.")
+                self.speak('Lo siento, ha ocurrido un error en la conexión.')
                     
     def abrir_mail(self):
-        """Funcion que abre el inbox de los mails"""
+        '''Funcion que abre el inbox de los mails'''
         url_mail = 'https://mail.google.com/mail/u/0/#inbox'
         webbrowser.open(url_mail)
         self.speak('Abriendo los mails...')
     
     def buscar_en_google(self):
-        """Funcion para buscar en google la consulta que se dicte"""
+        '''Funcion para buscar en google la consulta que se dicte'''
         recognizer = sr.Recognizer()
         with sr.Microphone() as source:
-            print("Esperando la consulta de búsqueda...")
+            print('Esperando la consulta de búsqueda...')
             audio = recognizer.listen(source, timeout=30)
         try:
             consulta = recognizer.recognize_google(audio, language='es-ES')
-            print("Consulta de búsqueda:", consulta)
-            url = f"https://www.google.com/search?q={consulta}"
+            print('Consulta de búsqueda:', consulta)
+            url = f'https://www.google.com/search?q={consulta}'
             webbrowser.open(url)
-            self.speak(f"Buscando {consulta} en Google.")
+            self.speak(f'Buscando {consulta} en Google.')
         except sr.UnknownValueError:
-            self.speak("Lo siento, no entendí la consulta de búsqueda.")
+            self.speak('Lo siento, no entendí la consulta de búsqueda.')
         except sr.RequestError:
-            self.speak("Lo siento, ha ocurrido un error en la conexión.")
+            self.speak('Lo siento, ha ocurrido un error en la conexión.')
                 
 
 luna = Asistente()
@@ -187,3 +197,4 @@ while luna.running:
     if not luna.running:
             break
 luna.despedir()
+
